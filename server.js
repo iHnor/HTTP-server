40259a86792cd611afba27901ac67d933c499518
Ihor Mykhno <igormykhno@gmail.com>
2021-07-28 17:50:13 +0300

    add error block

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 const server = http.createServer((require, response) => {
     if (require.url === '/headers'){
-
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Test server');
     }
     else {
         response.writeHead(400, 'Not Found');
